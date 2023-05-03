f7470005cf55c221f5946bd59ea51730a790cb0c
craig merry <craigm26@gmail.com>
2023-05-03 16:01:57 -0700

    profile update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonText, IonLabel } from '@ionic/react';
+import { useState, useEffect, useRef } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonText,
+  IonLabel,
+  IonChip,
+  IonAvatar,
+  IonImg,
+  IonButton,
+} from '@ionic/react';
 import './Profile.css';
-import useAuth from '../useAuth'; // Import useAuth hook
+import useAuth from '../useAuth';
+import 'firebase/compat/storage';
+import { storage } from '../firebaseConfig';
+import { ref, getDownloadURL, uploadBytesResumable, getStorage } from '@firebase/storage';
 
 const Profile: React.FC = () => {
-  const { user } = useAuth(); // Use the useAuth hook to get the user object
+  const { user } = useAuth();
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const renderUserInfo = () => {
-    if (user) {
-      return (
-        <div>
-          <p>{user.displayName || user.email}</p>
-          {user.isAnonymous && <p>(Anonymous user)</p>}
-        </div>
+  useEffect(() => {
+    if (user && user.uid) {
+      const storageRef = ref(storage, `avatars/${user.uid}`);
+
+      getDownloadURL(storageRef)
+        .then((url) => {
+          setAvatarUrl(url);
+        })
+        .catch((error) => {
+          console.error('Error fetching avatar:', error);
+        });
+    }
+  }, [user]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (user && user.uid && event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const storageRef = ref(storage, `avatars/${user.uid}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          console.log('Upload progress:', (snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        },
+        (error) => {
+          console.error('Error uploading avatar:', error);
+        },
+        () => {
+          getDownloadURL(storageRef).then((url) => {
+            setAvatarUrl(url);
+          });
+        },
       );
-    } else {
-      return <p>Loading...</p>;
     }
   };
 
@@ -28,13 +72,39 @@ const Profile: React.FC = () => {
           <IonText color="primary" class="BikeBusFont">
             <h1>BikeBus</h1>
           </IonText>
-          <IonLabel slot="end">
-            {renderUserInfo()} {/* Render user info */}
-          </IonLabel>
+          <IonChip slot="end">
+            {avatarUrl && (
+              <IonAvatar>
+                <IonImg src={avatarUrl} alt="User avatar" />
+              </IonAvatar>
+            )}
+            <IonLabel>{user?.displayName || user?.email}</IonLabel>
+          </IonChip>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonTitle size="large">Profile</IonTitle>
+        <div className="avatar-container">
+          {avatarUrl ? (
+            <IonAvatar>
+              <IonImg src={avatarUrl} alt="User avatar" />
+            </IonAvatar>
+          ) : (
+            <IonAvatar>
+              <IonImg src="/assets/icon/empty-avatar.svg" alt="Empty avatar" />
+            </IonAvatar>
+          )}
+          <input
+            type="file"
+            accept="image/*"
+            hidden
+            ref={fileInputRef}
+            onChange={handleFileChange}
+          />
+          <IonButton fill='clear' onClick={() => fileInputRef.current?.click()}>
+            {avatarUrl ? 'Change' : 'Add Avatar'}
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from '@firebase/auth';
+import { getStorage } from '@firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9KZ2IEQdvlBwfCocX-IxEJt3Q8fRNYz8",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { auth };
+export { auth, storage };
