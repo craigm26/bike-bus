8956cda9fc1613301afc6becfe9a5005de807bec
craig merry <craigm26@gmail.com>
2023-05-11 15:55:01 -0700

    SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,39 +4,17 @@ import { IonButton, IonInput, IonItem, IonLabel, IonPage, IonText } from '@ionic
 import useAuth from '../useAuth';
 import './Signup.css';
 import { db } from '../firebaseConfig';
-import { addDoc, collection, query, where, getDocs, CollectionReference, DocumentData, QuerySnapshot, doc, setDoc } from 'firebase/firestore';
-import { getAdditionalUserInfo } from '@firebase/auth';
-
-interface UserData {
-    uid: string;
-    email: string;
-    username: string;
-    firstName: string;
-    lastName: string;
-    accountType: string;
-    enabledAccountModes: string[];
-}
+import { collection, query, where, getDocs, doc, setDoc } from 'firebase/firestore';
 
 const Signup: React.FC = () => {
-    const { signUpWithEmailAndPassword, signInWithGoogle } = useAuth();
+    const { signUpWithEmailAndPassword } = useAuth();
     const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [isUsernameTaken, setIsUsernameTaken] = useState(false);
-
-
-    const createUserDocument = async (
-        usersCollectionRef: CollectionReference<DocumentData>,
-        newUserData: UserData
-    ) => {
-        const q = query(usersCollectionRef, where('uid', '==', newUserData.uid));
-        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(q);
-
-        if (querySnapshot.empty) {
-            await addDoc(usersCollectionRef, newUserData);
-        }
-    };
 
     const checkUsername = async (username: string) => {
         const usersCollectionRef = collection(db, 'users');
@@ -51,18 +29,18 @@ const Signup: React.FC = () => {
     };
 
 
-    const handleSignup = async (email: string, password: string, username: string) => {
+    const handleSignup = async (email: string, password: string, username: string, firstName: string, lastName:string) => {
         try {
             // Create the user in Firebase Authentication
-            const userCredential = await signUpWithEmailAndPassword(email, password, username);
+            const userCredential = await signUpWithEmailAndPassword(email, password, username, firstName, lastName);
             const user = userCredential.user;
             const userRef = doc(db, 'users', user.uid);
             await setDoc(userRef, {
                 username: username,
                 accountType: 'Member',
                 enabledAccountModes: ['Member'],
-                firstName: '',
-                lastName: '',
+                firstName: firstName,
+                lastName: lastName,
                 email: user.email,
             });
             // Redirect the user to the home page
@@ -94,6 +72,24 @@ const Signup: React.FC = () => {
             </IonItem>
 
             <IonItem>
+                <IonLabel position="floating">First Name</IonLabel>
+                <IonInput
+                    type="text"
+                    value={firstName}
+                    onIonChange={(event) => setFirstName(event.detail.value!)}
+                />
+            </IonItem>
+
+            <IonItem>
+                <IonLabel position="floating">Last Name</IonLabel>
+                <IonInput
+                    type="text"
+                    value={lastName}
+                    onIonChange={(event) => setLastName(event.detail.value!)}
+                />
+            </IonItem>
+
+            <IonItem>
                 <IonLabel position="floating">@Username</IonLabel>
                 <IonInput
                     type="text"
@@ -106,7 +102,7 @@ const Signup: React.FC = () => {
             </IonItem>
 
             <IonButton
-                onClick={() => handleSignup(email, password, username)}
+                onClick={() => handleSignup(email, password, username, firstName, lastName)}
                 disabled={isUsernameTaken}
             >
                 Sign Up
