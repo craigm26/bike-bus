1dbdad80615025e91313d245b137289751d678ac
craig merry <craigm26@gmail.com>
2023-05-19 15:09:48 -0700

    MapdirectionsUpdate

--- src/components/Mapping/LoadMap.tsx
+++ src/components/Mapping/LoadMap.tsx
@@ -6,7 +6,9 @@ import AvatarMapMarker from "../../components/AvatarMapMarker";
 import AnonymousAvatarMapMarker from "../../components/AnonymousAvatarMapMarker";
 import SearchDestination from "../../components/Mapping/SearchDestination";
 import "./LoadMap.css";
-import { IonToolbar } from "@ionic/react";
+import { IonButton } from "@ionic/react";
+import createRoute from "../../pages/createRoute";
+import CreateRoute from "../../pages/createRoute";
 
 const libraries: ("places" | "drawing" | "geometry" | "localContext" | "visualization")[] = ["places"];
 
@@ -16,6 +18,85 @@ interface LoadMapProps {
     user: { uid: string } | null;
     navigate: (path: string) => void;
 }
+
+// build the directions service
+const directionsService = new window.google.maps.DirectionsService();
+
+// build the directions renderer
+const directionsRenderer = new window.google.maps.DirectionsRenderer();
+
+// function to get directions
+const getDirections = () => {
+    // get the start and end locations
+    const start = { lat: 41.850033, lng: -87.6500523 };
+    const end = { lat: 41.8525800, lng: -87.6514100 };
+
+    // get the directions
+    directionsService.route(
+        {
+            origin: start,
+            destination: end,
+            travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+            if (status === window.google.maps.DirectionsStatus.OK && result?.routes[0]) {
+                // set the directions to the map
+                directionsRenderer.setDirections(result);
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        }
+    );
+};
+
+// function to create a route
+const createARoute = () => {
+    // get the start and end locations
+    const start = { lat: 41.850033, lng: -87.6500523 };
+    const end = { lat: 41.8525800, lng: -87.6514100 };
+
+    // get the directions
+    directionsService.route(
+        {
+            origin: start,
+            destination: end,
+            travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+            if (status === window.google.maps.DirectionsStatus.OK && result?.routes[0]) {
+                // set the directions to the map
+                directionsRenderer.setDirections(result);
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        }
+    );
+};
+
+// function to find a route
+const FindRoute = () => {
+    // get the start and end locations
+    const start = { lat: 41.850033, lng: -87.6500523 };
+    const end = { lat: 41.8525800, lng: -87.6514100 };
+
+    // get the directions
+    directionsService.route(
+        {
+            origin: start,
+            destination: end,
+            travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+            if (status === window.google.maps.DirectionsStatus.OK && result?.routes[0]) {
+                // set the directions to the map
+                directionsRenderer.setDirections(result);
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        }
+    );
+};
+
 
 
 const LoadMap: React.FC<LoadMapProps> = ({ mapCenter, isAnonymous, user, navigate }) => {
@@ -34,10 +115,22 @@ const LoadMap: React.FC<LoadMapProps> = ({ mapCenter, isAnonymous, user, navigat
                 }}
                 center={mapCenter}
                 zoom={16}
-                options={{}}
+                options={{
+                    disableDefaultUI: true,
+                    zoomControl: false,
+                    mapTypeControl: false,
+                    disableDoubleClickZoom: true,
+                    maxZoom: 18,
+
+                }}
             >
                 <div className="search-bar">
                     <SearchDestination currentLocation={mapCenter} navigate={navigate} />
+                    <div className="button-container">
+                        <IonButton color="primary" onClick={getDirections}>Get Directions</IonButton>
+                        <IonButton color="primary" onClick={createARoute}>Create Route</IonButton>
+                        <IonButton color="primary" onClick={FindRoute}>Find Route</IonButton>
+                    </div>
                 </div>
                 {user && isAnonymous && <AnonymousAvatarMapMarker position={mapCenter} uid={user.uid} />}
                 {user && !isAnonymous && <AvatarMapMarker uid={user.uid} position={mapCenter} />}
