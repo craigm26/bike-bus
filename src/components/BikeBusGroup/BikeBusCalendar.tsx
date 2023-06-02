bb8727aebb4e7bef3a23b28762ca27c8bde19faf
craig merry <craigm26@gmail.com>
2023-06-02 16:30:44 -0700

    Push Friday Build

--- src/components/BikeBusGroup/BikeBusCalendar.tsx
+++ src/components/BikeBusGroup/BikeBusCalendar.tsx
@@ -1,72 +1,74 @@
-import React, { useState, useEffect } from "react";
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import React, { useState, useEffect } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { getDoc, collection, getDocs, query, where, doc, DocumentReference } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
+import { useParams } from 'react-router-dom';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-interface Event {
-    start: Date;
-    end: Date;
-    title: string;
-}
+type Event = {
+    start: Date,
+    end: Date,
+};
 
-interface BikeBusCalendarProps {
-    schedules: any; // use a more specific type if possible
-}
-
-const BikeBusCalendar: React.FC<BikeBusCalendarProps> = ({ schedules }) => {
-// check to see if BikeBusSchedules is null or undefined. If it is, then set it to an empty string.
-    if (schedules === null || schedules === undefined) {
-        schedules = '';
-    }
+const BikeBusCalendar: React.FC = () => {
     const [events, setEvents] = useState<Event[]>([]);
+    const { groupId } = useParams<{ groupId: string }>();
+    console.log('groupId', groupId);
 
+    // get the schedules from the schedules collection where the BikeBusGroup is equal to the groupId
     useEffect(() => {
-        // Initialize the array to hold our events
-        const dailyEvents: Event[] = [];
-
-        // Generate an event for each weekday
-        for (let i = 0; i < 5; i++) {
-            const start = moment().startOf('week').add(i, 'days').hours(8).minutes(0);
-            const end = moment(start).add(1, 'hour'); 
-
-            dailyEvents.push({
-                start: start.toDate(),
-                end: end.toDate(),
-                title: 'Daily Event',
+        const fetchSchedules = async () => {
+            const schedulesCollection = collection(db, 'schedules');
+            const bikeBusGroupDocRef: DocumentReference = doc(db, 'bikebusgroups', groupId);
+            console.log('bikeBusGroupDocRef', bikeBusGroupDocRef);
+            const q = query(schedulesCollection, where('BikeBusGroup', '==', bikeBusGroupDocRef));
+            const querySnapshot = await getDocs(q);
+            console.log('querySnapshot', querySnapshot);
+            const fetchedEvents = querySnapshot.docs.map(doc => {
+                const data = doc.data();
+                return {
+                    start: new Date(data.startDateTime),
+                    end: new Date(data.endTime),
+                    title: data.scheduleName
+                };
             });
-        }
+            setEvents(fetchedEvents);
+            console.log('fetchedEvents', fetchedEvents);
+        };
+        fetchSchedules();
 
-        setEvents(dailyEvents);
-    }, []);
-
-    useEffect(() => {
-        // ... Convert scheduleData into the format required by the calendar
-        // Assuming scheduleData is not an array of schedules
-        if (schedules) {
-        // and each schedule has a start and end date, and a title
-        const convertedEvents = schedules.map((schedule: { startTime: string | number | Date; endTime: string | number | Date; scheduleName: any; }) => ({
-            start: new Date(schedule.startTime),
-            end: new Date(schedule.endTime),
-            title: schedule.scheduleName,
-        }));
-    
-        setEvents(convertedEvents);
+        // get the data from the bikebusgroups in firestore collection where the id is equal to the groupId
+        const fetchBikeBusGroup = async () => {
+            const bikeBusGroupDocRef: DocumentReference = doc(db, 'bikebusgroups', groupId);
+            const bikeBusGroupDoc = await getDoc(bikeBusGroupDocRef);
+            const bikeBusGroupData = bikeBusGroupDoc.data();
+            console.log('bikeBusGroupData', bikeBusGroupData);
         }
-    }, [schedules]);
-    
+        fetchBikeBusGroup();
+
+    }
+    , [groupId]);
 
     return (
-        <div>
+        <IonCard>
+            <IonCardHeader>
+                <IonCardTitle style={{ textAlign: 'center' }}>{}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+        <div style={{ height: 500 }}>
             <Calendar
                 localizer={localizer}
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
-                style={{ height: 500 }}
             />
         </div>
+            </IonCardContent>
+        </IonCard>
     );
 };
 
