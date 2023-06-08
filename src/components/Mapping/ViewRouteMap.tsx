989bf853f0cd185f8b7ad9bfcd6b9d12b88a976e
craig merry <craigm26@gmail.com>
2023-06-07 20:37:20 -0700

    view route updates

--- src/components/Mapping/ViewRouteMap.tsx
+++ src/components/Mapping/ViewRouteMap.tsx
@@ -23,7 +23,7 @@ interface ViewRouteMapProps {
 
 const containerMapStyle = {
     width: '100%',
-    height: '400px',
+    height: '100%',
 };
 
 const ViewRouteMap: React.FC<ViewRouteMapProps> = ({ startGeo, endGeo, stations, path }) => {
--- src/pages/ViewRoute.tsx
+++ src/pages/ViewRoute.tsx
@@ -7,23 +7,47 @@ import {
     IonButton,
     IonHeader,
     IonToolbar,
-    IonTitle,
+    IonCol,
+    IonGrid,
+    IonRow,
 } from '@ionic/react';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { useAvatar } from '../components/useAvatar';
 import { db } from '../firebaseConfig';
 import { HeaderContext } from "../components/HeaderContext";
-import { collection, deleteDoc, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
+import { DocumentReference, collection, deleteDoc, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
 import ViewRouteMap from '../components/Mapping/ViewRouteMap';
 import useAuth from "../useAuth";
 import { GeoPoint } from 'firebase/firestore';
 import { useParams, useHistory } from 'react-router-dom';
+import { GoogleMap, useJsApiLoader, Marker, Polyline } from '@react-google-maps/api';
+
+
+const libraries: ("places" | "drawing" | "geometry" | "localContext" | "visualization")[] = ["places"];
+
 
 interface Coordinate {
-    latitude: number;
-    longitude: number;
     lat: number;
     lng: number;
+}
+
+interface ViewRouteMapProps {
+    startGeo: GeoPoint;
+    endGeo: GeoPoint;
+    stations: Station[];
+    path: GeoPoint[];
+}
+
+interface BikeBusGroup {
+    id: string;
+    name: string;
+    description: string;
+    routeId: string;
+}
+
+interface Station {
+    id: string;
+    location: GeoPoint;
 }
 
 interface Route {
@@ -46,6 +70,12 @@ interface Route {
     isBikeBus: boolean;
 }
 
+interface Coordinate {
+    lat: number;
+    lng: number;
+}
+
+
 const ViewRoute: React.FC = () => {
     const { user } = useAuth();
     const history = useHistory();
@@ -56,32 +86,113 @@ const ViewRoute: React.FC = () => {
     const [selectedRoute, setSelectedRoute] = useState<Route | null>(null);
     const [routes, setRoutes] = useState<Route[]>([]);
     const { id } = useParams<{ id: string }>();
+    const [bikeBusGroup, setBikeBusGroup] = useState<BikeBusGroup | null>(null);
+    const [stations, setStations] = useState<Station[]>([]);
+    const { isLoaded, loadError } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? "",
+        libraries,
+    });
+    const [loading, setLoading] = useState(true);
+    const [path, setPath] = useState<GeoPoint[]>([]);
+    const [startGeo, setStartGeo] = useState<Coordinate>({ lat: 0, lng: 0 });
+    const [endGeo, setEndGeo] = useState<Coordinate>({ lat: 0, lng: 0 });    
+    const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>({
+        lat: startGeo.lat,
+        lng: startGeo.lng,
+    });
 
-    // retrieve the route from the database using the id from the url. 
+    const containerMapStyle = {
+        width: '100%',
+        height: '100%',
+    };
+
+
+
+    const fetchRoutes = useCallback(async () => {
+        const uid = user?.uid;
+        if (!uid) {
+            return;
+        }
+        const routesCollection = collection(db, 'routes');
+        const q = query(routesCollection, where("routeCreator", "==", `/users/${uid}`));
+        const querySnapshot = await getDocs(q);
+        const routesData: Route[] = querySnapshot.docs.map(doc => ({
+            ...doc.data() as Route,
+            id: doc.id,
+        }));
+        setRoutes(routesData);
+    }, [user]);
+
     useEffect(() => {
-        const getRoute = async () => {
-            const routeRef = doc(db, 'routes', id);
-            const routeSnapshot = await getDoc(routeRef);
-            if (routeSnapshot.exists()) {
-                const routeData = routeSnapshot.data();
-                if (routeData) {
-                    const route = {
-                        id: routeSnapshot.id,
-                        ...routeData
-                    } as Route;
-                    setSelectedRoute(route);
-                }
-            }
-        };
-        getRoute();
+        fetchRoutes();
+    }, [fetchRoutes]);
+
+    useEffect(() => {
+        if (id) fetchSingleRoute(id);
+    }, [id]);
+
+    const fetchSingleRoute = async (id: string) => {
+        const docRef = doc(db, 'routes', id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            const routeData = {
+                ...docSnap.data() as Route,
+                id: docSnap.id,
+                pathCoordinates: (docSnap.data().pathCoordinates || []).map((coord: any) => ({
+                    lat: coord.latitude,
+                    lng: coord.longitude,
+                })),
+            };
+            setSelectedRoute(routeData);
+        }
+    };
+
+    // make a new const to fetch the bikebusgroup name based on teh BikeBusGroupId in the route document
+    const fetchBikeBusGroup = async (id: string) => {
+        const docRef = doc(db, 'bikebusgroups', id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            const bikeBusGroupData = {
+                ...docSnap.data() as BikeBusGroup,
+                id: docSnap.id,
+            };
+            setBikeBusGroup(bikeBusGroupData);
+        }
+    };
+
+    console.log(selectedRoute?.BikeBusGroupId);
+
+    // if the route is in a bikebusgroup, then make the isGroup variable true
+    const isGroup = selectedRoute?.isBikeBus;
+
+    useEffect(() => {
+        if (selectedRoute) {
+            fetchBikeBusGroup(selectedRoute.id);
+        }
     }
-        , [id]);
+        , [selectedRoute]);
+
 
     useEffect(() => {
         if (headerContext) {
             headerContext.setShowHeader(true);
         }
     }, [headerContext]);
+
+    // center the map between the start point of the route and the end point of the route
+    useEffect(() => {
+        if (selectedRoute) {
+            setMapCenter({
+                lat: (selectedRoute.startPoint.lat + selectedRoute.endPoint.lat) / 2,
+                lng: (selectedRoute.startPoint.lng + selectedRoute.endPoint.lng) / 2,
+            });
+        }
+    }
+        , [selectedRoute]);
+
 
     useEffect(() => {
         if (user) {
@@ -102,71 +213,113 @@ const ViewRoute: React.FC = () => {
         if (selectedRoute) {
             const routeRef = doc(db, 'routes', selectedRoute.id);
             await deleteDoc(routeRef);
-            goToRouteList();
         }
     };
-    // go to the /viewroutelist/ page after deleting a route
-    const goToRouteList = () => {
-        history.push('/ViewRouteList');
-    };
-
-    console.log(selectedRoute)
 
     return (
-        <IonPage>
+        <IonPage style={{ height: '100%' }}>
             <IonHeader>
                 <IonToolbar>
                     {headerContext?.showHeader && <IonHeader></IonHeader>}
                 </IonToolbar>
             </IonHeader>
-            <IonContent>
-                <IonTitle>
-                    {selectedRoute?.routeName}
-                </IonTitle>
-                <IonList>
-                    <IonItem>
-                        <IonLabel>Route Name: {selectedRoute?.routeName}</IonLabel>
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel>Description: {selectedRoute?.description}</IonLabel>
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel>Travel Mode: {selectedRoute?.travelMode}</IonLabel>
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel>
-                            Starting Point: {selectedRoute?.startPointName}
-                        </IonLabel>
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel>
-                            Ending Point: {selectedRoute?.endPointName}
-                        </IonLabel>
-                    </IonItem>
-                    <IonItem>BikeBus Group: {selectedRoute?.BikeBusGroupId}</IonItem>
-                </IonList>
-                <IonButton routerLink={`/EditRoute/${id}`}>Edit Route</IonButton>
-                <IonButton onClick={deleteRoute}>Delete Route</IonButton>
-                <IonButton onClick={goToRouteList}>Go to Route List</IonButton>
-                <IonButton routerLink={`/CreateBikeBusGroup/${id}`}>Create BikeBus Group</IonButton>
+            <IonContent style={{ height: '100%' }}>
+                <IonGrid style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel>Route Name: {selectedRoute?.routeName}</IonLabel>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel>Description: {selectedRoute?.description}</IonLabel>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel>Travel Mode: {selectedRoute?.travelMode}</IonLabel>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel>
+                                Starting Point: {selectedRoute?.startPointAddress}
+                            </IonLabel>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel>
+                                Ending Point: {selectedRoute?.endPointAddress}
+                            </IonLabel>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            {!isGroup && (
+                                <IonLabel>
+                                    {bikeBusGroup?.name}
+                                </IonLabel>
+                            )}
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonButton routerLink={`/EditRoute/${id}`}>Edit Route</IonButton>
+                            <IonButton onClick={deleteRoute}>Delete Route</IonButton>
+                            <IonButton routerLink={'/ViewRouteList/'}>Go to Route List</IonButton>
+                        </IonCol>
+                    </IonRow>
+                    {isGroup && (
+                        <IonRow>
+                            <IonCol>
+                                <IonButton routerLink={`/CreateBikeBusGroup/${id}`}>Create BikeBus Group</IonButton>
+                            </IonCol>
+                        </IonRow>
+                    )}
+                    {selectedRoute && (
+                        <IonRow style={{ flex: '1' }}>
+                            <IonCol>
+                                <GoogleMap
+                                    mapContainerStyle={containerMapStyle}
+                                    center={mapCenter}
+                                    zoom={12}
+                                    options={{
+                                        mapTypeControl: false,
+                                        streetViewControl: false,
+                                        fullscreenControl: true,
+                                        disableDoubleClickZoom: true,
+                                        disableDefaultUI: true,
+                                    }}
+                                >
+                                    <Marker
+                                        position={{ lat: startGeo.lat, lng: startGeo.lng }}
+                                        title="Start"
+                                    />
+                                    <Marker
+                                        position={{ lat: endGeo.lat, lng: endGeo.lng }}
+                                        title="End"
+                                    />
+                                    <Polyline
+                                        path={path.map(geoPoint => ({ lat: geoPoint.latitude, lng: geoPoint.longitude }))}
+                                        options={{
+                                            strokeColor: "#FF0000",
+                                            strokeOpacity: 1.0,
+                                            strokeWeight: 2,
+                                            geodesic: true,
+                                            draggable: true,
+                                            editable: true,
+                                            visible: true,
+                                        }}
+                                    />
+                                </GoogleMap>
+                            </IonCol>
+                        </IonRow>
+                    )}
+                </IonGrid>
+            </IonContent>
+        </IonPage>
 
-
-                {selectedRoute && selectedRoute.startPoint && selectedRoute.endPoint && selectedRoute.pathCoordinates && (
-  <>
-    {console.log('startPoint:', selectedRoute.startPoint)}
-    {console.log('endPoint:', selectedRoute.endPoint)}
-    <ViewRouteMap
-      path={selectedRoute.pathCoordinates.map(coordinate => new GeoPoint(coordinate.latitude, coordinate.longitude))}
-      startGeo={new GeoPoint(selectedRoute.startPoint.latitude, selectedRoute.startPoint.longitude)}
-      endGeo={new GeoPoint(selectedRoute.endPoint.latitude, selectedRoute.endPoint.longitude)}
-      stations={[]}
-    />
-  </>
-)}
-
-
-            </IonContent >
-        </IonPage >
     );
 
 };
