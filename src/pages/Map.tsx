e27b7932ff7cf355025d8de3b155b6d2e46e0593
craig merry <craigm26@gmail.com>
2023-06-16 11:56:37 -0700

    text to create routes

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -15,6 +15,13 @@ import {
   IonModal,
   IonInput,
   IonTitle,
+  IonText,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonItem,
+  IonList,
 } from "@ionic/react";
 import { useEffect, useCallback, useState, useContext } from "react";
 import "./Map.css";
@@ -606,8 +613,38 @@ const Map: React.FC = () => {
         {!showMap && (
           <>
             <IonGrid>
-              <IonRow>
-                <IonCol className="location-button-container">
+              <IonRow className="location-button-container">
+                <IonCol className="location-app-help-container">
+                  <IonCard>
+                    <IonCardHeader>
+                      <IonCardTitle>How do I create a BikeBus?</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                      <IonList>
+                        <IonItem>1. First, you should be signed up as a User</IonItem>
+                        <IonItem>2. Then you create a route</IonItem>
+                        <IonItem>3. Create a BikeBus by clicking on the "Create BikeBus" button on the route page</IonItem>
+                        <IonItem>4. Invite users to your BikeBus</IonItem>
+                        <IonItem>5. When your schedule indicates that you (as BikeBus Leader) should start the BikeBus, Start the BikeBus on the Map</IonItem>
+                      </IonList>
+                    </IonCardContent>
+                  </IonCard>
+                  <IonCard>
+                    <IonCardHeader>
+                      <IonCardTitle>Creating a Route</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                      <IonList>
+                        <IonItem>1. Go to the Map Page</IonItem>
+                        <IonItem>2. Search for a Starting Location</IonItem>
+                        <IonItem>3. Search for a Destination</IonItem>
+                        <IonItem>4. Click on the "Get Directions" button</IonItem>
+                        <IonItem>5. Click on the "Create Route" button</IonItem>
+                      </IonList>
+                    </IonCardContent>
+                  </IonCard>
+                </IonCol>
+                <IonCol >
                   <IonButton onClick={getLocation}>Start Map by retrieving your Current Location</IonButton>
                 </IonCol>
               </IonRow>
