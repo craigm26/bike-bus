7d83f72a2c50b49928539a6e96010678a75bf43f
craig merry <craigm26@gmail.com>
2023-06-22 16:15:40 -0700

    schedule event url

--- src/pages/ViewSchedule.tsx
+++ src/pages/ViewSchedule.tsx
@@ -58,6 +58,9 @@ const ViewSchedule: React.FC = () => {
     const { id } = useParams<{ id: string }>();
     const [showEventModal, setShowEventModal] = useState(false);
     const [selectedEvent, setSelectedEvent] = useState<any>(null);
+    const [eventLink, setEventLink] = useState<string>('');
+
+
 
 
     useEffect(() => {
@@ -165,7 +168,6 @@ const ViewSchedule: React.FC = () => {
             setEvents(allEvents);
         };
         fetchSchedules();
-
     }, [events, id]);
 
     // write a new fetch for the event data in the firestore collection "event" by using the document id of the group
@@ -185,74 +187,12 @@ const ViewSchedule: React.FC = () => {
                 const eventDocSnapshot = await getDoc(eventDocRef);
                 const eventData = eventDocSnapshot.data();
 
-                if (!eventData) continue;
+                // for each event, create a reference to the /event/{eventDocRef.id} document
+                const eventDocRef2: DocumentReference = doc(db, 'event', eventDocRef.id);
+                const eventDocSnapshot2 = await getDoc(eventDocRef2);
+                const eventData2 = eventDocSnapshot2.data();
+                const eventLink = eventData2?.eventLink;
 
-                const title = eventData.title;
-                const startTime = eventData.startTime.split('T')[1];
-                const endTime = eventData.endTime.split('T')[1];
-
-                if (eventData.recurring === "yes") {
-                    for (let j = 0; j < eventData.eventDays.length; j++) {
-                        const eventDayTimestamp = eventData.eventDays[j];
-                        const eventDay = eventDayTimestamp.toDate(); // converts Timestamp to Date
-
-                        // Create date string in 'YYYY-MM-DD' format
-                        const year = eventDay.getFullYear();
-                        const month = String(eventDay.getMonth() + 1).padStart(2, '0'); // Months are 0 based
-                        const day = String(eventDay.getDate()).padStart(2, '0');
-                        const eventDayStr = `${year}-${month}-${day}`;
-
-                        const startDateTime = new Date(eventDayStr + 'T' + startTime);
-                        const endDateTime = new Date(eventDayStr + 'T' + endTime);
-
-                        const event: Event = {
-                            start: startDateTime,
-                            end: endDateTime,
-                            title: title,
-                            route: eventData.route,
-                            schedule: eventData.schedule,
-                            startTime: eventData.startTime,
-                            endTime: eventData.endTime,
-                            BikeBusGroup: eventData.BikeBusGroup,
-                            BikeBusStopTimes: eventData.BikeBusStopTimes,
-                            BikeBusStops: eventData.BikeBusStops,
-                            StaticMap: eventData.StaticMap,
-                            caboose: eventData.caboose,
-                            captains: eventData.captains,
-                            kids: eventData.kids,
-                            leader: eventData.leader,
-                            members: eventData.members,
-                            sheepdogs: eventData.sheepdogs,
-                            sprinters: eventData.sprinters,
-                            id: '',
-                        };
-                        eventDocs.push(event);
-                    }
-                } else {
-
-                    const event: Event = {
-                        start: startTime,
-                        end: endTime,
-                        title: title,
-                        route: eventData.route,
-                        schedule: eventData.schedule,
-                        startTime: eventData.startTime,
-                        endTime: eventData.endTime,
-                        BikeBusGroup: eventData.BikeBusGroup,
-                        BikeBusStopTimes: eventData.BikeBusStopTimes,
-                        BikeBusStops: eventData.BikeBusStops,
-                        StaticMap: eventData.StaticMap,
-                        caboose: eventData.caboose,
-                        captains: eventData.captains,
-                        kids: eventData.kids,
-                        leader: eventData.leader,
-                        members: eventData.members,
-                        sheepdogs: eventData.sheepdogs,
-                        sprinters: eventData.sprinters,
-                        id: '',
-                    };
-                    eventDocs.push(event);
-                }
             }
 
             setEvents(eventDocs);
@@ -264,7 +204,8 @@ const ViewSchedule: React.FC = () => {
     const handleSelectEvent = (event: Event) => {
         setSelectedEvent(event);
         setEvents(events);
-        setShowEventModal(true);
+        // make it appear in the Event box of the calendar
+        setEventLink(eventLink);
     };
 
 
@@ -290,25 +231,6 @@ const ViewSchedule: React.FC = () => {
                 </IonCard>
                 <IonButton routerLink={`/addschedule/${id}`}>Add Schedule</IonButton>
                 <IonButton routerLink={`/bikebusgrouppage/${id}`}>Back to BikeBusGroup</IonButton>
-                <IonModal isOpen={showEventModal} onDidDismiss={() => setShowEventModal(false)}>
-                    <IonHeader>
-                        <IonToolbar>
-                            <IonTitle></IonTitle>
-                        </IonToolbar>
-                    </IonHeader>
-                    <IonContent>
-                        <IonCard>
-                            <IonCardContent>
-                                <IonLabel>Start Time: {selectedEvent?.startTime}</IonLabel>
-                                <IonLabel>End Time: {selectedEvent?.endTime}</IonLabel>
-                                <IonLabel>Route: {events[0]?.route}</IonLabel>
-
-                            </IonCardContent>
-                        </IonCard>
-                        <IonButton routerLink={`/editevent/${selectedEvent?.id}`}>Edit Event</IonButton>
-                        <IonButton onClick={() => setShowEventModal(false)}>Close</IonButton>
-                    </IonContent>
-                </IonModal>
 
             </IonContent>
         </IonPage>
