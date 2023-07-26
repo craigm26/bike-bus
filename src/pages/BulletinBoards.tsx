f4db327eb89a21f79598eae3ccb563452e48ecc0
craig merry <craigm26@gmail.com>
2023-07-26 13:40:26 -0700

    added BulletinBoards

---
+++ src/pages/BulletinBoards.tsx
@@ -0,0 +1,354 @@
+import {
+    IonContent,
+    IonPage,
+    IonItem,
+    IonList,
+    IonInput,
+    IonLabel,
+    IonButton,
+    IonHeader,
+    IonToolbar,
+    IonCard,
+    IonCardContent,
+    IonCardTitle,
+    IonSelect,
+    IonSelectOption
+} from '@ionic/react';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { useAvatar } from '../components/useAvatar';
+import { db } from '../firebaseConfig';
+import { HeaderContext } from "../components/HeaderContext";
+import { DocumentReference, addDoc, arrayUnion, collection, doc, getDoc, getDocs, query, serverTimestamp, updateDoc, where, FieldValue } from 'firebase/firestore';
+import useAuth from "../useAuth";
+import Avatar from '../components/Avatar';
+
+interface Coordinate {
+    lat: number;
+    lng: number;
+}
+
+interface FirestoreRef {
+    path: string;
+}
+
+interface Organization {
+    id: string;
+    NameOfOrg: string;
+    accountType: string;
+    groupType: string;
+    description: string;
+    OrganizationCreator: string;
+    bulletinboard: FirestoreRef;
+}
+
+interface BikeBus {
+    id: string;
+    accountType: string;
+    groupType: string;
+    description: string;
+    endPoint: Coordinate;
+    BikeBusCreator: string;
+    BikeBusLeader: string;
+    BikeBusName: string;
+    BikeBusType: string;
+    startPoint: Coordinate;
+    travelMode: string;
+    bulletinboard: FirestoreRef;
+}
+
+interface SelectOption {
+    value: string;
+    label: string;
+}
+
+interface BulletinBoard {
+    Messages: DocumentReference[];
+}
+
+interface Message {
+    message: string;
+    user: {
+        id: string;
+        username: string;
+        avatarUrl: string;
+    } | null;
+    BikeBusGroup: DocumentReference | null;
+    Organization: DocumentReference | null;
+    timestamp: FieldValue;
+    bulletinboard: DocumentReference;
+}
+
+
+const BulletinBoards: React.FC = () => {
+    const { user } = useAuth();
+    const { avatarUrl } = useAvatar(user?.uid);
+    const headerContext = useContext(HeaderContext);
+    const [accountType, setAccountType] = useState<string>('');
+    const [groupType, setGroupType] = useState<string>('Organization');
+    const [combinedList, setCombinedList] = useState<SelectOption[]>([]);
+    const [selectedValue, setSelectedValue] = useState<string | undefined>('');
+    const [groupId, setGroupId] = useState<string | undefined>('');
+    const [groupData, setGroupData] = useState<any | undefined>(undefined);
+    const [username, setUsername] = useState<string | undefined>('');
+    const [messageInput, setMessageInput] = useState<string>('');
+    const [messagesData, setMessagesData] = useState<Message[]>([]);
+
+    useEffect(() => {
+        if (headerContext) {
+            headerContext.setShowHeader(true); // Hide the header for false, Show the header for true (default)
+        }
+    }, [headerContext]);
+
+    useEffect(() => {
+        if (user) {
+            const userRef = doc(db, 'users', user.uid);
+            getDoc(userRef).then((docSnapshot) => {
+                if (docSnapshot.exists()) {
+                    const userData = docSnapshot.data();
+                    if (userData && userData.accountType) {
+                        setAccountType(userData.accountType);
+                    }
+                    const username = userData?.username;
+                    if (username) {
+                        setUsername(username);
+                    }
+                } else {
+                }
+            });
+        }
+
+        if (selectedValue) {
+            const groupRef = doc(db, 'organizations', selectedValue);
+            getDoc(groupRef).then((docSnapshot) => {
+                if (docSnapshot.exists()) {
+                    const groupData = docSnapshot.data();
+                    if (groupData) {
+                        setGroupData(groupData);
+                        setGroupId(selectedValue);
+                    }
+                } else {
+                    const groupRef = doc(db, 'bikebusgroups', selectedValue);
+                    getDoc(groupRef).then((docSnapshot) => {
+                        if (docSnapshot.exists()) {
+                            const groupData = docSnapshot.data();
+                            if (groupData) {
+                                setGroupData(groupData);
+                                setGroupId(selectedValue);
+                            }
+                        } else {
+                        }
+                    });
+                }
+            });
+        }
+
+    }, [user, groupId, username, selectedValue]);
+
+    const fetchOrganizations = useCallback(async () => {
+        let formattedData: { value: string, label: string }[] = [];
+        try {
+            const uid = user?.uid;
+            console.log('UID:', uid);
+
+            if (!uid) {
+                return formattedData;
+            }
+
+            const userRef = doc(db, 'users', uid);
+            const OrganizationCollection = collection(db, 'organizations');
+            const q = query(OrganizationCollection, where('OrganizationMembers', 'array-contains', userRef));
+            const querySnapshot = await getDocs(q);
+
+            const OrganizationData: Organization[] = querySnapshot.docs.map(doc => ({
+                ...doc.data() as Organization,
+                id: doc.id,
+            }));
+
+            formattedData = OrganizationData.map(org => ({
+                value: org.id,
+                label: org.NameOfOrg
+            }));
+
+        } catch (error) {
+            console.log("Error fetching organizations:", error);
+        }
+
+        return formattedData;
+
+    }, [user]);
+
+
+    const fetchBikeBus = useCallback(async () => {
+        let formattedData: { value: string, label: string }[] = [];
+        const uid = user?.uid;
+        console.log('UID:', uid);
+
+        if (!uid) {
+            return formattedData;
+        }
+
+        const BikeBusCollection = collection(db, 'bikebusgroups');
+        const q = query(BikeBusCollection, where('BikeBusMembers', 'array-contains', doc(db, 'users', `${user?.uid}`)));
+        console.log('Query:', q);
+        const querySnapshot = await getDocs(q);
+
+        const BikeBusData: BikeBus[] = querySnapshot.docs.map(doc => ({
+            ...doc.data() as BikeBus,
+            id: doc.id,
+        }));
+
+        formattedData = BikeBusData.map(bus => ({
+            value: bus.id,
+            label: bus.BikeBusName
+        }));
+
+        return formattedData;
+    }, [user]);
+
+
+    const fetchMessages = useCallback(async () => {
+        if (groupData?.bulletinboard) {
+            const bulletinBoardRef = doc(db, groupData.bulletinboard.path);
+            const bulletinBoardDoc = await getDoc(bulletinBoardRef);
+    
+            if (bulletinBoardDoc.exists()) {
+                const bulletinBoardData = bulletinBoardDoc.data() as BulletinBoard;
+                const messagesData = bulletinBoardData?.Messages || [];
+    
+                const messagesPromises = messagesData.map(async (messageRef: DocumentReference) => {
+                    const messageDoc = await getDoc(messageRef);
+                    if (messageDoc.exists()) {
+                        const messageData = messageDoc.data();
+    
+                        if (messageData?.user) {
+                            const userDoc = await getDoc(messageData.user);
+                            const userData = userDoc.data();
+    
+                            return {
+                                ...messageData,
+                                user: userData ? {
+                                    ...userData,
+                                    id: userDoc.id,
+                                } : null,
+                            };
+                        } else {
+                        }
+                    } else {
+                    }
+                });
+    
+                const resolvedMessages = await Promise.all(messagesPromises);
+                setMessagesData(resolvedMessages.filter((item): item is Message => Boolean(item)));
+    
+            } else {
+            }
+        } else {
+        }
+    }, [groupData]);
+    
+
+    useEffect(() => {
+        if (groupType === 'BikeBus') {
+            Promise.all([fetchBikeBus()]).then(([bikebus]) => {
+                setCombinedList([...bikebus]);
+            });
+        } else {
+            Promise.all([fetchOrganizations(), fetchBikeBus()]).then(([orgs, bikebus]) => {
+                setCombinedList([...orgs, ...bikebus]);
+            });
+        }
+    }, [groupType, fetchOrganizations, fetchBikeBus]);
+
+    useEffect(() => {
+        fetchMessages();
+    }, [fetchMessages]);
+
+    const submitMessage = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (messageInput.trim() === '' || !user || !avatarUrl) {
+            return;
+        }
+
+        const bulletinBoardRef = doc(db, groupData.bulletinboard.path);
+        const userRef = doc(db, 'users', user.uid);
+
+        const messageRef = await addDoc(collection(db, 'messages'), {
+            message: messageInput,
+            user: userRef,
+            timestamp: serverTimestamp(),
+            bulletinboard: bulletinBoardRef,
+        });
+
+        await updateDoc(bulletinBoardRef, {
+            Messages: arrayUnion(messageRef),
+        });
+
+        setMessageInput('');
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    {headerContext?.showHeader && <IonHeader></IonHeader>}
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonCard>
+                    {combinedList.length === 0 ? (
+                        <IonCardContent>
+                            <IonCardTitle>How to Read Bulletin Boards:</IonCardTitle>
+                            <IonList>
+                                <IonItem>1. <IonButton className="ion-button-profile" fill="solid" routerLink="/Login">LogIn</IonButton></IonItem>
+                                <IonItem>2. Join a BikeBus <IonButton className="ion-button-profile" fill="solid" routerLink="/SearchForBikeBus">Search for BikeBus</IonButton> or Organization:  <IonButton className="ion-button-profile" fill="solid" routerLink="/SearchForOrganization">Search for Organization</IonButton></IonItem>
+                            </IonList>
+                        </IonCardContent>
+                    ) : (
+                        <IonCardContent>
+                            <IonCardTitle>Select Organization or BikeBus Bulletin Board:</IonCardTitle>
+                            <IonSelect value={selectedValue} placeholder="Select One" onIonChange={e => setSelectedValue(e.detail.value)}>
+                                {combinedList.map((item, index) => (
+                                    <IonSelectOption key={index} value={item.value}>
+                                        {item.label}
+                                    </IonSelectOption>
+                                ))}
+                            </IonSelect>
+                            <IonList>
+                                {selectedValue && (
+                                    <form onSubmit={submitMessage}>
+                                        <IonInput
+                                            value={messageInput}
+                                            placeholder="Enter your message"
+                                            onIonChange={e => setMessageInput(e.detail.value || '')}
+                                        />
+                                        <IonButton expand="full" type="submit">Post Bulletin Board Message</IonButton>
+                                    </form>
+                                )}
+                                {messagesData && messagesData.length > 0 && messagesData
+                                    .sort((a, b) => Number(b.timestamp) - Number(a.timestamp))
+                                    .map((message, index) => {
+                                        return (
+                                            <IonItem key={index}>
+                                                {username !== message?.user?.username && (
+                                                    <Avatar uid={message?.user?.id} size='extrasmall' />
+                                                )}
+                                                <IonLabel className={username === message?.user?.username ? 'right-align' : 'left-align'}>
+                                                    {message?.message}
+                                                </IonLabel>
+                                                {username === message?.user?.username && (
+                                                    <Avatar uid={message?.user?.id} size='extrasmall' />
+                                                )}
+                                            </IonItem>
+                                        );
+                                    })}
+                            </IonList>
+                        </IonCardContent>
+                    )}
+                </IonCard>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default BulletinBoards;
--- src/pages/ViewOrganizationList.tsx
+++ src/pages/ViewOrganizationList.tsx
@@ -9,6 +9,8 @@ import {
     IonHeader,
     IonToolbar,
     IonCard,
+    IonCardContent,
+    IonCardTitle,
 } from '@ionic/react';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { useAvatar } from '../components/useAvatar';
@@ -62,20 +64,20 @@ const ViewOrganizationList: React.FC = () => {
     const fetchOrganizations = useCallback(async () => {
         const uid = user?.uid;
         console.log('UID:', uid);
-    
+
         if (!uid) {
             return;
         }
-    
+
         // Fetch the user's document reference
         const userRef = doc(db, 'users', uid);
-    
+
         const OrganizationCollection = collection(db, 'organizations');
         // Use the user's document reference in the query
         const q = query(OrganizationCollection, where('OrganizationMembers', 'array-contains', userRef));
-    
+
         const querySnapshot = await getDocs(q);
-    
+
         const OrganizationData: Organization[] = querySnapshot.docs.map(doc => ({
             ...doc.data() as Organization,
             id: doc.id,
@@ -83,7 +85,7 @@ const ViewOrganizationList: React.FC = () => {
         console.log('OrganizationData:', OrganizationData);
         setOrganization(OrganizationData);
     }, [user]);
-    
+
 
 
     useEffect(() => {
@@ -102,14 +104,38 @@ const ViewOrganizationList: React.FC = () => {
             </IonHeader>
             <IonContent>
                 <IonCard>
-                    <IonList>
-                        {Organization.map((Organization) => (
-                            <IonItem key={Organization.id}>
-                                <IonLabel>{Organization.NameOfOrg}</IonLabel>
-                                <IonButton routerLink={`/ViewOrganization/${Organization.id}`}>View Organization</IonButton>
-                            </IonItem>
-                        ))}
-                    </IonList>
+                    {Organization.length === 0 ? (
+                        <IonCardContent>
+                            <IonCardTitle>How to Create or Join an Organization</IonCardTitle>
+                            <IonLabel>Organizations are a specific group of people who manage multiple BikeBus groups and routes.</IonLabel>
+                            <IonLabel>Organizations can be created and joined by anyone with a BikeBus account. Everyone is a member; a lot of functions are based on higher privileges .</IonLabel>
+                            <IonLabel>When you create an Organization, you become the Organization's Administrator.</IonLabel>
+                            <IonLabel>As an Administrator, you can create BikeBus groups and routes for your Organization.</IonLabel>
+                            <IonLabel>As an Administrator, you can invite other people to join your Organization.</IonLabel>
+                            <IonList>
+                                <IonLabel>To Create an Organization:</IonLabel>
+                                <IonItem>1. <IonButton className="ion-button-profile" fill="solid" routerLink="/Login">LogIn</IonButton></IonItem>
+                                <IonItem>2. Create a Organization:  <IonButton className="ion-button-profile" fill="solid" routerLink="/CreateOrganization">Create Organization</IonButton></IonItem>
+                                <IonItem>3. Invite people to join your Organization:  <IonButton className="ion-button-profile" fill="solid" routerLink="/Help">Help: Inviting People to Join Your Organization</IonButton></IonItem>
+                                <IonItem>4. Delegate roles to your Organization:</IonItem>
+                                <IonItem>5. Create BikeBus groups and routes for your Organization:</IonItem>
+                                <IonItem>6. Train others on timekeeping and reporting</IonItem>
+                            </IonList>
+                        </IonCardContent>
+                    ) : (
+                        <IonCardContent>
+                            <p>You are a member of the following Organizations:</p>
+                            <IonList>
+                                {Organization.map((Organization) => (
+                                    <IonItem key={Organization.id}>
+                                        <IonLabel>{Organization.NameOfOrg}</IonLabel>
+                                        <IonButton routerLink={`/ViewOrganization/${Organization.id}`}>View Organization</IonButton>
+                                    </IonItem>
+                                ))}
+                            </IonList>
+                        </IonCardContent>
+                    )}
+
                 </IonCard>
             </IonContent>
         </IonPage>
