17ad082bea1e50984ab43c94332a1ef5e8a8538a
craig merry <craigm26@gmail.com>
2023-09-15 13:02:00 -0700

    map cluster on map home

--- src/components/BikeBusDirectory.tsx
+++ src/components/BikeBusDirectory.tsx
@@ -1,23 +1,17 @@
 import {
-    IonContent,
-    IonPage,
     IonItem,
     IonList,
     IonLabel,
     IonButton,
-    IonHeader,
-    IonToolbar,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardTitle,
 } from '@ionic/react';
 import { useCallback, useContext, useEffect, useState } from 'react';
-import { useAvatar } from '../components/useAvatar';
 import { db } from '../firebaseConfig';
 import { HeaderContext } from "../components/HeaderContext";
-import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
+import { DocumentReference, collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
 import useAuth from "../useAuth";
+import { GoogleMap, InfoWindow, Marker, Polyline, useJsApiLoader, MarkerClusterer } from "@react-google-maps/api";
+const libraries: ("places" | "drawing" | "geometry" | "localContext" | "visualization")[] = ["places"];
+
 
 
 
@@ -31,6 +25,7 @@ interface BikeBus {
     accountType: string;
     description: string;
     endPoint: Coordinate;
+    BikeBusRoutes: DocumentReference[];
     BikeBusCreator: string;
     BikeBusLeader: string;
     BikeBusName: string;
@@ -39,11 +34,47 @@ interface BikeBus {
     travelMode: string;
 }
 
+interface RouteData {
+    eventCheckInLeader: any;
+    startPoint: { lat: number; lng: number };
+    endPoint: { lat: number; lng: number };
+    pathCoordinates: { lat: number; lng: number }[];
+    startPointName: string;
+    endPointName: string;
+    startPointAddress: string;
+    endPointAddress: string;
+    routeName: string;
+    routeType: string;
+    routeCreator: string;
+    routeLeader: string;
+    description: string;
+    travelMode: string;
+    isBikeBus: boolean;
+    BikeBusName: string;
+    BikeBusStopName: string[];
+    BikeBusStop: Coordinate[];
+    BikeBusStops: Coordinate[];
+    BikeBusStationsIds: string[];
+    BikeBusGroupId: DocumentReference;
+    BikeBusStopIds: DocumentReference[];
+    id: string;
+    accountType: string;
+    routeId: string;
+    name: string;
+  }
+
 const BikeBusDirectory: React.FC = () => {
     const { user } = useAuth();
-    const headerContext = useContext(HeaderContext);
     const [accountType, setaccountType] = useState<string>('');
     const [BikeBus, setBikeBus] = useState<BikeBus[]>([]);
+    const [route, setRoute] = useState<RouteData>();
+    const [routeId, setRouteId] = useState<string>('');
+    const [endPointAddress, setEndPointAddress] = useState<string>('');
+    const [endPoint, setEndPoint] = useState<Coordinate>();
+    const [cityGroups, setCityGroups] = useState<Record<string, BikeBus[]>>({});
+    const [RouteArray, setRouteArray] = useState<RouteData[]>([]);
+
+
 
     useEffect(() => {
         if (user) {
@@ -61,26 +92,38 @@ const BikeBusDirectory: React.FC = () => {
 
     const fetchBikeBus = useCallback(async () => {
         const uid = user?.uid;
-        console.log('UID:', uid);
-
+    
         if (!uid) {
             return;
         }
-
+    
         const BikeBusCollection = collection(db, 'bikebusgroups');
         const q = query(BikeBusCollection, where('BikeBusMembers', 'array-contains', doc(db, 'users', `${user?.uid}`)));
-        console.log('Query:', q);
         const querySnapshot = await getDocs(q);
-
-
-
+    
         const BikeBusData: BikeBus[] = querySnapshot.docs.map(doc => ({
             ...doc.data() as BikeBus,
             id: doc.id,
         }));
-        console.log('BikeBusData:', BikeBusData);
         setBikeBus(BikeBusData);
+    
+        const RouteArrayTemp: RouteData[] = [];
+    
+        for (const bikeBusDoc of querySnapshot.docs) {
+            const BikeBusRoutesRef = bikeBusDoc.data().BikeBusRoutes;
+    
+            if (Array.isArray(BikeBusRoutesRef)) {
+                for (const routeRef of BikeBusRoutesRef) {
+                    const routeDoc = await getDoc(routeRef as DocumentReference);
+                    const routeData = routeDoc.data() as RouteData;
+                    RouteArrayTemp.push(routeData);
+                }
+            }
+        }
+    
+        setRouteArray(RouteArrayTemp);
     }, [user]);
+    
 
 
     useEffect(() => {
@@ -88,18 +131,60 @@ const BikeBusDirectory: React.FC = () => {
         fetchBikeBus();
     }, [fetchBikeBus, user]);
 
+    const extractCity = (address: string): string => {
+        const parts = address?.split(",");
+        if (parts && parts[1]) {
+            return parts[1].trim();
+        }
+        return "Unknown City"; // fallback
+    };
+    
 
+    console.log("RouteArray:", RouteArray);
+    console.log("BikeBusArray:", BikeBus);
+
+    console.log("Length of RouteArray:", RouteArray.length);
+    console.log("Length of BikeBusArray:", BikeBus.length);
+
+
+
+    const groupByCity = (BikeBusArray: BikeBus[], RouteArray: RouteData[]): Record<string, BikeBus[]> => {
+        return BikeBusArray.reduce((acc: Record<string, BikeBus[]>, curr: BikeBus, index: number) => {
+            const routeInfo = RouteArray[index];
+            if (!routeInfo || !routeInfo.endPointAddress) {
+                console.error(`Missing route info for index ${index}.`);
+                return acc;
+            }
+            const city = extractCity(routeInfo.endPointAddress);
+            if (!acc[city]) {
+                acc[city] = [];
+            }
+            acc[city].push(curr);
+            return acc;
+        }, {});
+    };
+    
+
+    const BikeBusByCity = groupByCity(BikeBus, RouteArray);
 
     return (
-                <IonList>
-                    {BikeBus.map((BikeBus) => (
-                        <IonItem key="id">
-                            <IonLabel>{BikeBus.BikeBusName}</IonLabel>
-                            <IonButton routerLink={`/bikebusgrouppage/${BikeBus.id}`}>View BikeBus</IonButton>
-                        </IonItem>
-                    ))}
-                </IonList>
-
+        <div>
+            {Object.keys(BikeBusByCity).map((city, index) => (
+                <div key={index}>
+                    <h2>{city}</h2>
+                    <IonList>
+                        {BikeBusByCity[city].map((BikeBus) => (
+                            <IonItem key={BikeBus.id}>
+                                <IonLabel>{BikeBus.BikeBusName}</IonLabel>
+                                <IonButton routerLink={`/bikebusgrouppage/${BikeBus.id}`}>
+                                    View BikeBus
+                                </IonButton>
+                            </IonItem>
+                        ))}
+                    </IonList>
+                </div>
+            ))}
+        </div>
     );
 };
 
