519eb0ac185fd444868325e1a0949e7aa2215647
craig merry <craigm26@gmail.com>
2023-10-06 10:55:42 -0700

    update appdelegate error handling for ios

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,10 +8,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    do {
         FirebaseApp.configure()
-        return true
+    } catch let error {
+        print("Error configuring Firebase: \(error)")
     }
+    return true
+    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
