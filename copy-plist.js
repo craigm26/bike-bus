7cca3a54e3dee9f41a9eee1cb985ac28aedc5033
craig merry <craigm26@gmail.com>
2023-10-06 10:31:30 -0700

    revert back to non config xml and plist scripting

--- copy-plist.js
+++
@@ -1,9 +0,0 @@
-// scripts/copy-plist.js
-const fs = require('fs');
-const path = require('path');
-
-const srcPath = path.resolve(__dirname, '../GoogleService-Info.plist');
-const destPath = path.resolve(__dirname, '../ios/App/App/GoogleService-Info.plist');
-
-fs.copyFileSync(srcPath, destPath);
-console.log('GoogleService-Info.plist copied to iOS platform');
